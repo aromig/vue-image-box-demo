a10468553468ed0e28db4ad3b8f47d6082dfb269
Adam Romig <adam.romig@gmail.com>
2019-04-24 15:45:54 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,29 +1,67 @@
 # vue-image-box
 
-## Project setup
-```
-npm install
+Lightweight and simple-ish image gallery component for Vue.js.
+
+Images can be cycled throgh using the left & right arrow keys, and closed with the escape key.
+
+## Using this Component
+
+1. Save the `vue-image-box.vue` file into your project's "components" folder (e.g. `src/components/`).
+2. Import to the appropriate vue file (such as `App.vue`) and register the component into your application.
+
+## Example usage
+
+This component works by waiting for `index` to be populated with a non-null value. Images are constructed from an array of URLs (`images`) and the click handler updates the value of `index`, triggering the display of the larger modal of the image.
+
+Vue Template:
+
+```html
+<template>
+  <div>
+    <img
+      v-for="(image, idx) in images"
+      :key="idx"
+      :src="image"
+      @click="onClick(idx)"
+      class="gallery__image"
+    />
+    <ImageBox :images="images" :index="index" @close="index = null"></ImageBox>
+  </div>
+</template>
 ```
 
-### Compiles and hot-reloads for development
-```
-npm run serve
+JavaScript:
+
+```javascript
+import ImageBox from "@/components/vue-image-box.vue";
+
+export default {
+  name: "app",
+  components: {
+    ImageBox
+  },
+  methods: {
+    onClick: function(idx) {
+      this.index = idx;
+    }
+  },
+  data() {
+    return {
+      index: null,
+      images: [
+        "https://placekitten.com/800/800",
+        "https://placekitten.com/825/800",
+        "https://placekitten.com/803/800"
+      ]
+    };
+  }
+};
 ```
 
-### Compiles and minifies for production
-```
-npm run build
-```
+## Author
 
-### Run your tests
-```
-npm run test
-```
+Adam Romig
 
-### Lints and fixes files
-```
-npm run lint
-```
+## License
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+vue-image-box is available under the MIT license. See the LICENSE file for details.
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Vue Image Box</h1>
+    <div class="gallery">
+      <img
+        v-for="(image, idx) in images"
+        :key="idx"
+        :src="image"
+        @click="onClick(idx)"
+        class="gallery__image"
+      />
+      <ImageBox
+        :images="images"
+        :index="index"
+        @close="index = null"
+      ></ImageBox>
+    </div>
+    <img src="@/assets/logo.png" alt="Vue Logo" class="vue-logo" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import ImageBox from "@/components/vue-image-box.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    ImageBox
+  },
+  methods: {
+    onClick: function(idx) {
+      this.index = idx;
+    }
+  },
+  data() {
+    return {
+      index: null,
+      images: [
+        "https://placekitten.com/800/800",
+        "https://placekitten.com/825/800",
+        "https://placekitten.com/803/800",
+        "https://placekitten.com/804/800",
+        "https://placekitten.com/805/800",
+        "https://placekitten.com/806/800",
+        "https://placekitten.com/807/800",
+        "https://placekitten.com/808/800",
+        "https://placekitten.com/832/800",
+        "https://placekitten.com/811/800",
+        "https://placekitten.com/873/800",
+        "https://placekitten.com/895/800"
+      ]
+    };
   }
 };
 </script>
@@ -23,6 +61,30 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.vue-logo {
+  display: block;
+  width: 50px;
+  margin: 1rem auto;
+}
+
+.gallery {
+  display: grid;
+  grid-gap: 20px;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-auto-rows: auto;
+  grid-auto-flow: row dense;
+  &__image {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    box-shadow: -2px 2px 10px 0px rgba(#444, 0.4);
+    transition: transform 0.3s ease-in-out;
+    cursor: pointer;
+    &:hover {
+      transform: scale(1.05);
+    }
+  }
 }
 </style>
